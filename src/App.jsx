3086ea28229a5cbb3bb1701cb9f5889d80644771
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-07-17 16:52:59 +0100

    showing the status section when the the game is over

--- src/App.jsx
+++ src/App.jsx
@@ -71,9 +71,20 @@ export default function App() {
                 <h1 className=" font-medium text-xl text-[#F9F4DA] ">Assembly: Endgame</h1>
                 <p className=" font-medium text-sm text-[#8E8E8E] max-w-88 ">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
             </div>
-            <div className=" bg-[#10A95B] text-[#F9F4DA] font-medium text-center w-88 rounded-sm py-1 mt-5 ">
-                <h2 className=" text-xl ">You win!</h2>
-                <p className=" text-sm ">Well done! 🎉</p>
+            <div className=" mt-5 ">
+                {
+                    isGameOver ? 
+                        isGameWon ? 
+                            <div className=" bg-key-right text-[#F9F4DA] font-medium text-center w-88 rounded-sm py-1 ">
+                                <h2 className=" text-xl ">You win!</h2>
+                                <p className=" text-sm ">Well done! 🎉</p>
+                            </div> :
+                            <div className=" bg-key-wrong text-[#F9F4DA] font-medium text-center w-88 rounded-sm py-1 ">
+                                <h2 className=" text-xl ">Game over!</h2>
+                                <p className=" text-sm ">You lose! Better start learning Assembly 😭</p>
+                            </div>
+                    : <div className="h-14"></div>
+                }
             </div>
             <div className=" flex flex-wrap max-w-66.5 gap-0.5 justify-center mt-10 ">
                 {languagesChips}
